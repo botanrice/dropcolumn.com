b48a4066aeb3df021019bba2edcf38ee31f1fb40
botanrice <tiobenito.music@gmail.com>
2020-05-11 17:43:53 -0400

    reverting homepage

---
+++ build/precache-manifest.b45b98d9834e4ce40738a6f3434ae875.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afd445de9ef1679186dbc9942661f7b0",
+    "url": "/dropcolumn.com/index.html"
+  },
+  {
+    "revision": "b0640f7cd1945048ba74",
+    "url": "/dropcolumn.com/static/css/main.c642c57f.chunk.css"
+  },
+  {
+    "revision": "11c066284d3bb285e0be",
+    "url": "/dropcolumn.com/static/js/2.be89564a.chunk.js"
+  },
+  {
+    "revision": "b0640f7cd1945048ba74",
+    "url": "/dropcolumn.com/static/js/main.e3bbab12.chunk.js"
+  },
+  {
+    "revision": "b6f45a6c511873760b97",
+    "url": "/dropcolumn.com/static/js/runtime-main.8c99ad09.js"
+  },
+  {
+    "revision": "8efbf7e25b6a84c5430544b9d3f9a8fb",
+    "url": "/dropcolumn.com/static/media/LM_black.8efbf7e2.png"
+  }
+]);
