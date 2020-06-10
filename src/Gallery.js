17e3c05ddb3da5351ab26390d36f5fb3d24f3fc8
botanrice <tiobenito.music@gmail.com>
2020-06-10 17:15:27 -0400

    implemented sidebar w gallery and about placeholders

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,14 +1,30 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image'
 import './assets/stylesheets/Gallery.css';
+import LMBlack from './assets/LM_black.png';
 
 
 export default class Gallery extends React.Component {
 
   render() {
     return (
-      <div className="gallery-body">
-        <h1>Gallery</h1>
-      </div>
+      <Container className="gallery-body" fluid>
+          <h1>Gallery</h1>
+        <Row>
+          <Col md={4}>
+            <Image src={LMBlack} fluid className="galleryImage" />
+          </Col>
+          <Col md={4}>
+            <Image src={LMBlack} fluid className="galleryImage" />
+          </Col>
+          <Col md={4}>
+            <Image src={LMBlack} fluid className="galleryImage" />
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
