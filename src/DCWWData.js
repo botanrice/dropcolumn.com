369b648a463c47b8a9006c4514e6e0e9d113aa29
botanrice <tiobenito.music@gmail.com>
2022-05-12 15:24:22 -0400

    randomized margins and profile buttons

--- src/DCWWData.js
+++ src/DCWWData.js
@@ -1,59 +1,82 @@
+import beagle2 from "./assets/images/propics/beagle2.png";
+import benjo from "./assets/images/propics/benjo.png";
+import jones from "./assets/images/propics/jones.png";
+import carbrey from "./assets/images/propics/carbrey.png";
+import mo from "./assets/images/propics/morice.png";
+import occlude from "./assets/images/propics/occlude2.png";
+import osiris from "./assets/images/propics/osiris.png";
+import otoroshi from "./assets/images/propics/otoroshi.jpg";
+import whet from "./assets/images/propics/whet.png";
+import stoic from "./assets/images/stoic.png";
+import noFake from "./assets/images/stoic.png";
+
 
 let dcwwData = [
   {
     name: "Jones Avenue",
     id: 100,
     link: "https://soundcloud.com/jones_avenue",
+    img: jones,
   },
   {
     name: "Beagle",
     id: 101,
     link: "https://soundcloud.com/beagle31",
+    img: beagle2,
   },
   {
     name: "noFake",
     id: 102,
     link: "https://soundcloud.com/nofakez",
+    img: noFake,
   },
   {
     name: "KingOsiris",
     id: 103,
     link: "https://soundcloud.com/kingosirisj",
+    img: osiris,
   },
   {
     name: "whet",
     id: 104,
     link: "https://soundcloud.com/allwhetwhetwhet",
+    img: whet,
   },
   {
     name: "Occlude",
     id: 105,
     link: "https://soundcloud.com/itsocclude",
+    img: occlude,
   },
   {
     name: "Carbrey",
     id: 106,
     link: "https://soundcloud.com/carbaeiry",
+    img: carbrey,
   },
   {
     name: "Benjo",
     id: 107,
     link: "https://soundcloud.com/dropcolumnworldwide/benjo-more-jewels-than-king",
+    img: benjo,
   },
   {
     name: "Mo",
     id: 108,
     link: "https://soundcloud.com/user-534316836",
+    img: mo,
   },
   {
     name: "Otoroshi",
     id: 109,
     link: "https://soundcloud.com/otoroshi",
+    img: otoroshi,
   },
   {
     name: "stoic da poet",
     id: 110,
     link: "https://soundcloud.com/stoicdamc",
+    img: stoic,
   },
 ];
 
--- src/routes/Secret.js
+++ src/routes/Secret.js
@@ -1,11 +1,15 @@
 import styled from 'styled-components';
 import { Link, Outlet } from "react-router-dom";
 import { getDCWWData } from "../DCWWData";
+import beagle2 from "../assets/images/propics/beagle2.png";
 
 const SecretDiv = styled.div`
   font-family: monospace;
   color: black;
   margin: 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 
 const StyledLink = styled.a`
@@ -94,19 +98,74 @@ const LinkStyle = {
   "text-align": "center",
 }
 
+const SecretFlexDiv = styled(SecretDiv)`
+  font-family: monospace;
+  color: black;
+  margin: 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const ProfileButton = styled.div.attrs((props) => ({ 
+  topMargin: Math.round(Math.random() * 200),
+  bottomMargin: Math.round(Math.random() * 200),
+  leftMargin: Math.round(Math.random() * 500),
+  rightMargin: Math.round(Math.random() * 500),
+}))`
+  border: 3px solid black;
+  border-radius: 50px;
+  width: 100px;
+  height: 100px;
+  margin-top: ${props => props.topMargin}px;
+  margin-bottom: ${props => props.bottomMargin}px;
+  margin-left: ${props => props.leftMargin}px;
+  margin-right: ${props => props.rightMargin}px;
+  // margin: 5px;
+  /* Button attributes */
+  cursor: pointer;
+  box-shadow: -5px 5px 0px 0px #9a9999;
+  // transition: color 0.5s ease-in-out 0s;
+
+  &:active {
+    transform: translateX(-3px) translateY(3px);
+    box-shadow: -2px 2px 0px 0px #9a9999;
+  }
+;`
+
+const ProfileImg = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  border-radius: 50px;
+`;
+
+
 export default function Secret() {
   let homies = getDCWWData();
   return (
-    <SecretDiv>
-      <SecretDiv>
+    <SecretDiv className="SecretContainer">
+      <SecretDiv className="SecretButtonContainer">
         {homies.map((homie) => {
           return (
-            <div>
-              <StyledLink href={homie.link}>{homie.name}</StyledLink>
-            </div>
+            <ProfileButton key={homie.name}>
+              <ProfileImg src={homie.img} alt={homie.name} title={homie.name} />  
+            </ProfileButton>
           );
         })}
       </SecretDiv>
+      <hr />
+      <hr />
+      <hr />
+      {/* <SecretDiv className="SecretButtonContainer">
+        {homies.map((homie) => {
+          return (
+            <ExtraStyledButton key={homie.name}>{homie.name}</ExtraStyledButton>
+          );
+        })}
+      </SecretDiv> */}
     </SecretDiv>
   );
 }
+
+// {/* <StyledLink href={homie.link}>{homie.name}</StyledLink> */}
